548183fc9d8697eaade71b0d936c379bc0efd2d6
curder <q.curder@gmail.com>
2021-02-20 22:22:52 +0800

    create ProductList and shop.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <product-list></product-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ProductList from "./components/ProductList";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    ProductList,
   }
 }
 </script>
